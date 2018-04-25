d8dbc0775fcf08dd43d828a71095cea2ebb350de
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-25 16:33:53 +0200

    Remove serve-icon import and declaration

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -9,8 +8,6 @@ const routes = require('../config/routes');
 const app = express();
 app.use('/', routes);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,7 +25,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: req.app.get('env') === 'development' ? err : {}
+    error: req.app.get('env') === 'development' ? err : {},
   });
 });
 
