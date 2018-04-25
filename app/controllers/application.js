54b9426d488e386eacf645a2befd720f9379a030
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-25 16:34:36 +0200

    Add Post Index route and controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,11 @@
 const appController = {
   getIndex: (req, res) => {
     res.send('Hello world!');
-  }
+  },
+  postIndex: (req, res) => {
+    console.log('Hello', process.env.SLACK_TOKEN);
+    res.send('See you slack');
+  },
 };
 
 module.exports = appController;
--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const c = require('../app/controllers');
 
-router.route('/').get(c.application.getIndex);
+router
+  .route('/')
+  .get(c.application.getIndex)
+  .post(c.application.postIndex);
 
 module.exports = router;
