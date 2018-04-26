cb6fef6ef4135685e5fd5ef20b4230aafa2e59fc
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-26 16:32:29 +0200

    Add fetch user info and repos

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -9,28 +9,53 @@ const msg = {
 function sendMessage(text, attachments) {
   msg.text = text;
   if (attachments) msg.attachments = [attachments];
-  console.log(msg);
 
   return msg;
 }
 
-function formatUserInfo(user) {
-  const text = `
-    *bio*: ${user.bio}\n*company*: ${user.company}\n*email*: ${
-    user.email
-  }\n*location*: ${user.location}\n*# of repos*: ${
-    user.public_repos
-  }\n*# of followers*: ${user.followers}
-  `;
+function formatUserInfo(data, command) {
+  let text = '';
+  let pretext;
+  let authorName;
+  let authorLink;
 
-  const attachment = {
-    author_name: user.name,
-    author_link: user.html_url,
+  if (command) {
+    switch (command) {
+      case 'repos':
+        authorName = data.length > 0 ? data[0].owner.name : '';
+        authorLink = data.length > 0 ? data[0].owner.html_url : '';
+        pretext =
+          data.length > 0 ? `${data[0].owner.login}'s list of repos:` : '';
+
+        data.forEach((repo, i) => {
+          text += `${i}. ${repo.html_url}\n`;
+          return text;
+        });
+        break;
+      default:
+        break;
+    }
+  } else {
+    authorName = data.name;
+    authorLink = data.html_url;
+    pretext = `Git info for user *${data.login}*`;
+    text += `
+       *bio*: ${data.bio || '-'}
+      *company*: ${data.company || '-'}
+      *email*: ${data.email || '-'}
+      *location*: ${data.location || '-'}
+      *# of repos*: ${data.public_repos}
+      *# of followers*: ${data.followers}
+  `;
+  }
+
+  return {
+    pretext,
+    author_name: authorName,
+    author_link: authorLink,
     color: '#764FA5',
     text,
   };
-
-  return attachment;
 }
 
 function getGit(res, user, command) {
@@ -57,8 +82,7 @@ function getGit(res, user, command) {
       throw new Error('User not found');
     })
     .then(data => {
-      console.log(data);
-      res.json(sendMessage(`Git info for *${user}*`, formatUserInfo(data)));
+      res.json(sendMessage('', formatUserInfo(data, command)));
     })
     .catch(err => console.log(err));
 }
@@ -81,49 +105,4 @@ const appController = {
   },
 };
 
-/*
-req { token: 'fKKPtrFftQYvtI4q8kGSkZfL',
-  team_id: 'TA5C77UKB',
-  team_domain: 'denisatk',
-  channel_id: 'CA5U98S3U',
-  channel_name: 'general',
-  user_id: 'UA6QZBLHL',
-  user_name: 'denis.atkesone',
-  command: '/getgit',
-  text: 'user command',
-  response_url: 'https://hooks.slack.com/commands/TA5C77UKB/353077653745/USFJO5e176sCPjXn9YJkXIXa' }
- */
-
-/*
-{ login: 'kheyro',
-  id: 10345706,
-  avatar_url: 'https://avatars2.githubusercontent.com/u/10345706?v=4',
-  gravatar_id: '',
-  url: 'https://api.github.com/users/kheyro',
-  html_url: 'https://github.com/kheyro',
-  followers_url: 'https://api.github.com/users/kheyro/followers',
-  following_url: 'https://api.github.com/users/kheyro/following{/other_user}',
-  gists_url: 'https://api.github.com/users/kheyro/gists{/gist_id}',
-  starred_url: 'https://api.github.com/users/kheyro/starred{/owner}{/repo}',
-  subscriptions_url: 'https://api.github.com/users/kheyro/subscriptions',
-  organizations_url: 'https://api.github.com/users/kheyro/orgs',
-  repos_url: 'https://api.github.com/users/kheyro/repos',
-  events_url: 'https://api.github.com/users/kheyro/events{/privacy}',
-  received_events_url: 'https://api.github.com/users/kheyro/received_events',
-  type: 'User',
-  site_admin: false,
-  name: 'Denis A.',
-  company: null,
-  blog: '',
-  location: null,
-  email: null,
-  hireable: true,
-  bio: null,
-  public_repos: 422,
-  public_gists: 3,
-  followers: 1,
-  following: 1,
-  created_at: '2014-12-30T04:13:41Z',
-  updated_at: '2018-04-09T20:13:59Z' }
- */
 module.exports = appController;
